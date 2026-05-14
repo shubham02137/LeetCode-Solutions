36f6d67b431fda3c73d4f913ea4ba051a16899b1
SHUBHAM KUMAR <shubhamkumar02137@gmail.com>
2026-05-14 15:27:00 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 43.7 MB (21.16%)

---
+++ solutions/0019-remove-nth-node-from-end-of-list/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        // Dummy node to handle edge cases
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // Move fast pointer n+1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        // Move both pointers
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // Delete the target node
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
