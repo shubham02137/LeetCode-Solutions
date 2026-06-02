35578f089fe70fe22133606ee65afee36022a3dc
SHUBHAM KUMAR <shubhamkumar02137@gmail.com>
2026-06-02 06:08:24 +0000

    [LeetCode] Runtime - 3 ms (56.86%), Memory - 46.9 MB (61.05%)

---
+++ solutions/3965-earliest-finish-time-for-land-and-water-rides-i/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
+                                  int[] waterStartTime, int[] waterDuration) {
+
+        int ans = Integer.MAX_VALUE;
+
+        int n = landStartTime.length;
+        int m = waterStartTime.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                // Land -> Water
+                int landFinish = landStartTime[i] + landDuration[i];
+                int waterStart = Math.max(landFinish, waterStartTime[j]);
+                int finish1 = waterStart + waterDuration[j];
+
+                // Water -> Land
+                int waterFinish = waterStartTime[j] + waterDuration[j];
+                int landStart = Math.max(waterFinish, landStartTime[i]);
+                int finish2 = landStart + landDuration[i];
+
+                ans = Math.min(ans, Math.min(finish1, finish2));
+            }
+        }
+
+        return ans;
+    }
+}
