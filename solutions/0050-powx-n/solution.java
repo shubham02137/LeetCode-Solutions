f6d9b48b96a58bb8c7e5bd52b100fedfaa9860b4
SHUBHAM KUMAR <shubhamkumar02137@gmail.com>
2026-07-02 19:53:34 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 47.7 MB (78.92%)

---
+++ solutions/0050-powx-n/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n;   // Convert to long to handle INT_MIN
+        
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1.0;
+
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                result *= x;
+            }
+
+            x *= x;
+            power >>= 1;
+        }
+
+        return result;
+    }
+}
