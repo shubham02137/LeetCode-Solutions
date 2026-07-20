856bca7fabfcd66c2c288ce0a3378106b3c664c9
SHUBHAM KUMAR <shubhamkumar02137@gmail.com>
2026-07-20 16:10:37 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 46.5 MB (49.91%)

---
+++ solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            // Take every positive price increase
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
