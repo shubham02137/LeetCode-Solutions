144ffecc4c46c7718908d0b9e5234c2cf182eb47
SHUBHAM KUMAR <shubhamkumar02137@gmail.com>
2026-07-23 14:29:59 +0000

    [LeetCode] Runtime - 28 ms (13.21%), Memory - 44.2 MB (77.41%)

---
+++ solutions/0133-clone-graph/solution.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+
+    private Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+
+        if (node == null) {
+            return null;
+        }
+
+        // Already cloned
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        // Create clone
+        Node clone = new Node(node.val);
+
+        // Store before DFS to handle cycles
+        map.put(node, clone);
+
+        // Clone all neighbors
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return clone;
+    }
+}
