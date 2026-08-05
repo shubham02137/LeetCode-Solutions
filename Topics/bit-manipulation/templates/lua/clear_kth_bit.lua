54f0842ded743013aed9325cbce6b3cb9e81b271
SHUBHAM KUMAR <shubhamkumar02137@gmail.com>
2026-08-05 20:21:15 +0530

    Delete Topics/bit-manipulation/templates/lua directory

--- Topics/bit-manipulation/templates/lua/clear_kth_bit.lua
+++
@@ -1,6 +0,0 @@
----@param num integer
----@param k integer
----@return integer
-function clear_kth_bit(num, k)
-    return num & ~(1 << k)
-end
--- Topics/bit-manipulation/templates/lua/test_kth_bit.lua
+++
@@ -1,6 +0,0 @@
----@param num integer
----@param k integer
----@return boolean
-function test_kth_bit(num, k)
-    return num & (1 << k) != 0
-end
